488b1e18f4b5715d25e379452e627cec06598427
JMEIgor <igor@jme.net.br>
2024-07-07 19:02:27 -0300

    remove fool directories

--- jme_ai_broken/documentation/v_cdr_transcriptions.sql
+++
@@ -1,7 +0,0 @@
-v_cdr_transcriptions
-- id => id da tabela
-- uniqueid => uniqueid
-- spekaer => qual dos lados que falou
-- start => início em segundos
-- end => fim em segundos
-- text => texto falado
